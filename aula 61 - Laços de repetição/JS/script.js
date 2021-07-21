fa2f2dc4a95e08f45c27b5f9b1c1f1c6594f16cf
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-07-20 22:34:06 -0300

    muitas aulas só hoje.

---
+++ aula 61 - Laços de repetição/JS/script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    var contador = 0
+
+    /*      WHILE - PRECISAMOS DE UMA VARIAVEL INICIAL 
+    while(contador <= 10){
+        console.log(` Número: ${contador}`)
+        contador++
+    }
+    */
+   /*Mesma coisa que o While, mas de baixo para cima:*/
+   do {
+    console.log(` Número: ${contador}`)
+    contador++
+   } while (contador <= 10);
+
+}
---
+++ aula 62 - Funções/JS/script.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    /*function realizarConta(num1=0,num2=0){
+        var som = num1 + num2
+        console.log('Iremos realizar uma função !')
+        console.log('A soma de '+num1+' e '+num2+' é de: '+som)
+    }
+
+    realizarConta(10,20)*/
+    
+    /*function teste(num1,num2){
+        return num1 + num2;
+    }
+    var variavel = teste(10,20);
+    console.log(variavel)*/
+
+    var obj = {'nome':'Guilherme','idade':27}; /*DECLARANDO UM OBJETO */
+        console.log(obj.nome)
+
+}
