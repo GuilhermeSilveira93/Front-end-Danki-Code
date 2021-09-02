e771e095d986dc56e34a340f1d19c619b476cd68
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-09-02 20:32:59 -0300

    aula 71
    manipulando as dimensões.

--- JQUERY/aula 71 - Manipulando dimensões dinamicamente/js/functions.js
+++ JQUERY/aula 71 - Manipulando dimensões dinamicamente/js/functions.js
@@ -1,7 +1,22 @@
 
 $(window).on('load', function(){ /*MÉTODO QUE FAZ O SCRIPT CARREGAR DEPOIS DA PAGINA*/
-   var elWidth = $('.box').width();
+   
+   //$('.box').width('900'); FORMA INCORRETA 
 
-   console.log(elWidth);
+   //SETANDO LARGURA 
+   $('.box').css('width','900')
+
+   // MOSTRANDO LARGURA
+   console.log('Width: '+$('.box').width());
+
+   console.log('InnerWidth: '+$('.box').innerWidth());
+   
+   console.log('outerWidth: '+$('.box').outerWidth(true));
+
+   console.log('Height: '+$('.box').height());
+
+   console.log('InnerHeight: '+$('.box').innerHeight());
+   
+   console.log('outerHeight: '+$('.box').outerHeight(true));
 
 })
